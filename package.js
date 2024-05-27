9fa47808b3ab52a66954f0e5b294bc511d1f5047
Jan Dvorak <jdvorak@literaryuniverse.com>
2024-05-27 21:01:17 +0200

    Update to 3.0-rc.2

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 /* global Package */
 Package.describe({
-    name: 'socialize:messaging',
-    summary: 'A social messaging package',
-    version: '2.0.0',
-    git: 'https://github.com/copleykj/socialize-messaging.git',
-});
+  name: 'socialize:messaging',
+  summary: 'A social messaging package',
+  version: '2.0.0',
+  git: 'https://github.com/copleykj/socialize-messaging.git'
+})
 
 Package.onUse(function _(api) {
-    api.versionsFrom(['2.8.1', '3.0-rc.0']);
+  api.versionsFrom(['2.8.1', '3.0-rc.2'])
 
-    api.use([
-        'check',
-        'aldeed:simple-schema@1.13.1',
-        'socialize:user-presence@2.0.0',
-        'socialize:linkable-model@2.0.0',
-        'reywood:publish-composite@1.8.9',
-    ]);
+  api.use([
+    'check',
+    'aldeed:simple-schema@1.13.1',
+    'socialize:user-presence@2.0.0',
+    'socialize:linkable-model@2.0.0',
+    'reywood:publish-composite@1.8.9'
+  ])
 
-    api.mainModule('server.js', 'server');
-    api.mainModule('common.js', 'client');
-});
+  api.mainModule('server.js', 'server')
+  api.mainModule('common.js', 'client')
+})
